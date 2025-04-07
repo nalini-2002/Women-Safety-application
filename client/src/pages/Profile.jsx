933b2018cef5951fcfdf30b1c2b9b35dd63baec6
vignesh <vigneshphr2002@gmail.com>
2025-04-07 21:14:51 +0530

    changes updated

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -84,7 +84,7 @@ const Profile = () => {
               <div className="card">
                 <div className="card-body">
                   <div className="d-flex flex-column align-items-center text-center">
-                    <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" className="rounded-circle" width={150} />
+                    <img src="https://i.ibb.co/svdYvXfw/woman.png" alt="Admin" className="rounded-circle" width={150} />
                     <div className="mt-3">
                       <h4>{auth?.user?.uname}</h4>
                       <p className="text-muted font-size-sm">Pincode : {auth?.user?.pinCode}</p>
